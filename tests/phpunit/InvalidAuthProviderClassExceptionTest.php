beef394856f013acff34ec7d0b00314cb028df48
Marijn van Wezel <marijnvanwezel@gmail.com>
2020-03-10 16:40:59 +0100

    Remove unit test automation

--- tests/phpunit/InvalidAuthProviderClassExceptionTest.php
+++ tests/phpunit/InvalidAuthProviderClassExceptionTest.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once __DIR__ . '/bootstrap.php';
-
 /**
  * Class InvalidAuthProviderClassExceptionTest
  *
--- tests/phpunit/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once __DIR__ . '/../../src/WSOAuth.php';
-include_once __DIR__ . '/../../src/AuthProviderFramework.php';
-include_once __DIR__ . '/../../src/AuthProvider.php';
-include_once __DIR__ . '/../../src/Exception/UnknownAuthProviderException.php';
-include_once __DIR__ . '/../../src/Exception/InvalidAuthProviderClassException.php';
-include_once __DIR__ . '/../../src/AuthenticationProvider/MediaWikiAuth.php';
